40e03b88f58f832c7b5c79c3ffa46f15efd71d6b
Akihiro <lbycnxrrxn765v+git@gmail.com>
2016-04-11 03:50:32 +0900

    .

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,11 @@ gulp.task("sass", function() {
 });
 
 gulp.task("js", function () {
-     return gulp.src(['resources/js/**/*.js','bower_components/bootstrap/dist/js/bootstrap.min.js']†)
+     return gulp.src(['bower_components/jquery/dist/jquery.min.js','bower_components/bootstrap/dist/js/bootstrap.min.js','resources/js/**/*.js'])
          .pipe( concat("app.js") )
          .pipe( gulp.dest("public/js") );
      console.log('JSを結合しました。');
      gulp.watch('./resources/js/*.js', ['js']);
  });
+
+gulp.task('default', ['sass','js']);
